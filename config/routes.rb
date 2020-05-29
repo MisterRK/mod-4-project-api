509b6d69904e6d7ac0f3121997b6a95456a03948
MisterC1ean <robert.keller44@icloud.com>
2020-05-29 13:30:35 -0400

    backend ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :gifs
-  resources :users
-  resources :scores
+  namespace :api do
+    namespace :v1 do
+      resources :gifs
+      resources :users
+      resources :scores
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 User.create(user_name: "Robert")
 User.create(user_name: "Alex")
+User.create(user_name: "Ethan")
+User.create(user_name: "Yueying")
+User.create(user_name: "Brett")
+User.create(user_name: "Isabella")
+
+Score.create(user_id: 1, value: 10000)
+Score.create(user_id: 1, value: 15000)
+
+Score.create(user_id: 2, value: 10000)
+Score.create(user_id: 2, value: 15000)
+
+Score.create(user_id: 3, value: 10000)
+Score.create(user_id: 3, value: 15000)
+
+Score.create(user_id: 4, value: 10000)
+Score.create(user_id: 4, value: 15000)
+
+Score.create(user_id: 5, value: 10000)
+Score.create(user_id: 5, value: 15000)
+
+Score.create(user_id: 6, value: 10000)
+Score.create(user_id: 6, value: 15000)
 
 Score.create(user_id: 1, value: 10000)
 Score.create(user_id: 1, value: 15000)
