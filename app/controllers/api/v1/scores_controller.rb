c792f35a3b595f3acd843bc0f859f96bfdf60e86
Asilver-jpg <asilver7493@gmail.com>
2020-06-30 18:05:10 -0400

    posts high score now

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -5,4 +5,11 @@ class Api::V1::ScoresController < ApplicationController
     render json: scores
   end
 
+  def create
+    score= {value: params["score"], user_id: params["id"]}
+    Score.create(score)
+    render json: score
+  end
+
+  
 end
